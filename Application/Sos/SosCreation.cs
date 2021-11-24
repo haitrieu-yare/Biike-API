b98e0f7687d323b68162ac4790346da16190a313
haitrieu-yare <haitrieu.yare@gmail.com>
2021-11-24 20:39:44 +0700

    small fix

--- Application/Sos/SosCreation.cs
+++ Application/Sos/SosCreation.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
 using Application.Sos.DTOs;
 using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
@@ -45,6 +47,16 @@ namespace Application.Sos
                 {
                     cancellationToken.ThrowIfCancellationRequested();
 
+                    var totalRecord = await _context.Sos.Where(s => s.UserId == request.UserId)
+                        .CountAsync(cancellationToken);
+
+                    if (totalRecord > 3)
+                    {
+                        _logger.LogInformation("This user has reach the maximum number of sos (3) can be created");
+                        return Result<Unit>.Failure(
+                            "This user has reach the maximum number of sos (3) can be created.");
+                    }
+
                     Domain.Entities.Sos newSos = new();
 
                     _mapper.Map(request.SosCreationDto, newSos);
@@ -62,8 +74,7 @@ namespace Application.Sos
                     }
 
                     _logger.LogInformation("Successfully created new sos");
-                    return Result<Unit>.Success(Unit.Value, "Successfully created new sos.", 
-                        newSos.SosId.ToString());
+                    return Result<Unit>.Success(Unit.Value, "Successfully created new sos.", newSos.SosId.ToString());
                 }
                 catch (Exception ex) when (ex is TaskCanceledException)
                 {
