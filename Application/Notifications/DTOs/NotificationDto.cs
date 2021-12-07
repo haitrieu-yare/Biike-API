c47267f8ee60afc605e75ed4803a28787b309ea9
haitrieu-yare <haitrieu.yare@gmail.com>
2021-12-07 23:23:30 +0700

    Push notification

--- Application/Notifications/DTOs/NotificationDto.cs
+++ Application/Notifications/DTOs/NotificationDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Application.Notifications.DTOs
@@ -6,7 +7,7 @@ namespace Application.Notifications.DTOs
     public class NotificationDto
     {
         [JsonPropertyName("notificationId")] public Guid? NotificationId { get; set; }
-        [JsonPropertyName("receiverId")] public int? ReceiverId { get; set; }
+        [Required] [JsonPropertyName("receiverId")] public int? ReceiverId { get; set; }
         [JsonPropertyName("title")] public string? Title { get; set; }
         [JsonPropertyName("content")] public string? Content { get; set; }
         [JsonPropertyName("url")] public string? Url { get; set; }
--- Application/Notifications/NotificationSending.cs
+++ Application/Notifications/NotificationSending.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Notifications.DTOs;
 using Firebase.Database;
 using Firebase.Database.Query;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace Application.Notifications
 {
     public class NotificationSending
     {
+        private readonly DataContext _context;
         private readonly IConfiguration _configuration;
         private readonly ILogger<NotificationSending> _logger;
 
-        public NotificationSending(IConfiguration configuration, ILogger<NotificationSending> logger)
+        public NotificationSending(DataContext context, IConfiguration configuration, ILogger<NotificationSending> logger)
         {
+            _context = context;
             _configuration = configuration;
             _logger = logger;
         }
@@ -37,8 +44,47 @@ namespace Application.Notifications
                 await firebaseClient.Child("notification")
                     .Child($"{notification.ReceiverId}")
                     .PostAsync(notificationJsonString);
-            
+
                 _logger.LogInformation("Successfully sent notification to {ReceiverId}", notification.ReceiverId);
+
+                const string requestUrl = "https://fcm.googleapis.com/fcm/send";
+
+                var user = await _context.User.FindAsync(notification.ReceiverId!);
+                
+                if (user == null || user.IsDeleted)
+                {
+                    _logger.LogError("Can't find user with UserId {request.UserId} " +
+                                     "to send push notification", notification.ReceiverId);
+                    return;
+                }
+
+                var pushNotification = new
+                {
+                    To = user.LastLoginDevice,
+                    Notification = new
+                    {
+                        Sound = "default",
+                        Body = notification.Content,
+                        Title = notification.Title,
+                        ContentAvailable = true,
+                        Priority = "high",
+                    },
+                };
+
+                string requestBody = JsonSerializer.Serialize(pushNotification, options);
+                HttpContent stringContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+                HttpClient client = new();
+                client.DefaultRequestHeaders.Authorization =
+                    AuthenticationHeaderValue.Parse($"key={_configuration["Firebase:CloudMessagingSecret"]}");
+
+                HttpResponseMessage task =
+                    await client.PostAsync(requestUrl, stringContent, CancellationToken.None);
+
+                if (!task.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Failed to send push notification");
+                }
             }
             catch (Exception e)
             {
